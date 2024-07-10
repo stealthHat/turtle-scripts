36f0e4d7487eab27bac132d402cd93668ad45b30
Guilherme <blinklgui@gmail.com>
2024-07-10 19:06:27 -0300

    update

--- tasks/quarry/control_plane.lua
+++ tasks/quarry/control_plane.lua
@@ -24,9 +24,9 @@ local function make_jobs()
   for x = x_min, x_max, width do
     for z = z_min, z_max, width do
       local job = {
-        x = x - width,
+        x = x,
         y = y_start,
-        z = z - width,
+        z = z,
         width = width,
         depth = depth,
       }
